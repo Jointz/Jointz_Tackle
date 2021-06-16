638c7fd2528e8c166ce4d916efbf6e2607938c2b
Jointz <86015398+Jointz@users.noreply.github.com>
2021-06-16 18:15:18 +0100

    Add files via upload

--- __resource.lua
+++ __resource.lua
@@ -1,15 +1,15 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'Badger_Tackle - enables tackling for cops'
+description 'Jointz_Tackle - enables tackling for cops'
 
 version '1.0.0'
 
 server_scripts {
 	'config.lua',
-	'server/main.lua'
+	'sv_main.lua'
 }
 
 client_scripts {
 	'config.lua',
-	'client/main.lua'
+	'cl_main.lua'
 }
